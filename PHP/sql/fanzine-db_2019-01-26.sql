b81b953563ae4c64b3241bbb564afacf1956aa21
Marc Augier <m.augier@me.com>
2019-01-26 12:16:46 +0100

    Mise à jour SQL fanzine
    il manquait les champs pseudo et passe à la table utilisateur

--- PHP/sql/fanzine-db_2019-01-26.sql
+++ PHP/sql/fanzine-db_2019-01-26.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 4.6.6
+-- version 4.8.2
 -- https://www.phpmyadmin.net/
 --
--- Client :  127.0.0.1:3406
--- Généré le :  Jeu 24 Janvier 2019 à 07:49
--- Version du serveur :  5.6.35
--- Version de PHP :  7.1.1
+-- Host: localhost:3306
+-- Generation Time: Jan 26, 2019 at 11:15 AM
+-- Server version: 5.7.21
+-- PHP Version: 7.2.7
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
@@ -17,13 +17,15 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- Base de données :  `fanzine`
+-- Database: `fanzine`
 --
+CREATE DATABASE IF NOT EXISTS `fanzine` DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci;
+USE `fanzine`;
 
 -- --------------------------------------------------------
 
 --
--- Structure de la table `compose`
+-- Table structure for table `compose`
 --
 
 CREATE TABLE `compose` (
@@ -34,7 +36,7 @@ CREATE TABLE `compose` (
 -- --------------------------------------------------------
 
 --
--- Structure de la table `createur`
+-- Table structure for table `createur`
 --
 
 CREATE TABLE `createur` (
@@ -48,7 +50,7 @@ CREATE TABLE `createur` (
 -- --------------------------------------------------------
 
 --
--- Structure de la table `fan_x_createur`
+-- Table structure for table `fan_x_createur`
 --
 
 CREATE TABLE `fan_x_createur` (
@@ -59,7 +61,7 @@ CREATE TABLE `fan_x_createur` (
 -- --------------------------------------------------------
 
 --
--- Structure de la table `fan_x_oeuvre`
+-- Table structure for table `fan_x_oeuvre`
 --
 
 CREATE TABLE `fan_x_oeuvre` (
@@ -70,7 +72,7 @@ CREATE TABLE `fan_x_oeuvre` (
 -- --------------------------------------------------------
 
 --
--- Structure de la table `oeuvre`
+-- Table structure for table `oeuvre`
 --
 
 CREATE TABLE `oeuvre` (
@@ -80,17 +82,10 @@ CREATE TABLE `oeuvre` (
   `oeuvre_type_id` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
 
---
--- Contenu de la table `oeuvre`
---
-
-INSERT INTO `oeuvre` (`oeuvre_id`, `oeuvre_nom`, `oeuvre_description`, `oeuvre_type_id`) VALUES
-(1, 'La joconde', '	', 2);
-
 -- --------------------------------------------------------
 
 --
--- Structure de la table `oeuvre_x_createur`
+-- Table structure for table `oeuvre_x_createur`
 --
 
 CREATE TABLE `oeuvre_x_createur` (
@@ -101,7 +96,7 @@ CREATE TABLE `oeuvre_x_createur` (
 -- --------------------------------------------------------
 
 --
--- Structure de la table `type`
+-- Table structure for table `type`
 --
 
 CREATE TABLE `type` (
@@ -109,54 +104,48 @@ CREATE TABLE `type` (
   `type_nom` varchar(60) COLLATE utf8_bin NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
 
---
--- Contenu de la table `type`
---
-
-INSERT INTO `type` (`type_id`, `type_nom`) VALUES
-(1, 'musique'),
-(2, 'peinture');
-
 -- --------------------------------------------------------
 
 --
--- Structure de la table `utilisateur`
+-- Table structure for table `utilisateur`
 --
 
 CREATE TABLE `utilisateur` (
   `utilisateur_id` int(11) NOT NULL,
+  `utilisateur_pseudo` varchar(60) COLLATE utf8_bin NOT NULL,
+  `utilisateur_passe` varchar(250) COLLATE utf8_bin NOT NULL,
   `utilisateur_prenom` varchar(80) COLLATE utf8_bin NOT NULL,
   `utilisateur_nom` varchar(80) COLLATE utf8_bin NOT NULL,
   `utilisateur_email` varchar(80) COLLATE utf8_bin NOT NULL,
-  `utilisateur_avatar_url` varchar(200) COLLATE utf8_bin NOT NULL
+  `utilisateur_avatar_url` varchar(200) COLLATE utf8_bin DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
 
 --
--- Index pour les tables exportées
+-- Indexes for dumped tables
 --
 
 --
--- Index pour la table `compose`
+-- Indexes for table `compose`
 --
 ALTER TABLE `compose`
   ADD PRIMARY KEY (`groupe_id`,`artiste_id`),
   ADD KEY `createur_enfant` (`artiste_id`);
 
 --
--- Index pour la table `createur`
+-- Indexes for table `createur`
 --
 ALTER TABLE `createur`
   ADD PRIMARY KEY (`createur_id`);
 
 --
--- Index pour la table `fan_x_createur`
+-- Indexes for table `fan_x_createur`
 --
 ALTER TABLE `fan_x_createur`
   ADD PRIMARY KEY (`utilisateur_id`,`createur_id`),
   ADD KEY `createur` (`createur_id`);
 
 --
--- Index pour la table `fan_x_oeuvre`
+-- Indexes for table `fan_x_oeuvre`
 --
 ALTER TABLE `fan_x_oeuvre`
   ADD PRIMARY KEY (`utilisateur_id`,`oeuvre_id`),
@@ -164,81 +153,85 @@ ALTER TABLE `fan_x_oeuvre`
   ADD KEY `oeuvre` (`oeuvre_id`);
 
 --
--- Index pour la table `oeuvre`
+-- Indexes for table `oeuvre`
 --
 ALTER TABLE `oeuvre`
   ADD PRIMARY KEY (`oeuvre_id`),
   ADD KEY `type_oeuvre` (`oeuvre_type_id`);
 
 --
--- Index pour la table `oeuvre_x_createur`
+-- Indexes for table `oeuvre_x_createur`
 --
 ALTER TABLE `oeuvre_x_createur`
   ADD PRIMARY KEY (`oeuvre_id`,`createur_id`);
 
 --
--- Index pour la table `type`
+-- Indexes for table `type`
 --
 ALTER TABLE `type`
   ADD PRIMARY KEY (`type_id`);
 
 --
--- Index pour la table `utilisateur`
+-- Indexes for table `utilisateur`
 --
 ALTER TABLE `utilisateur`
   ADD PRIMARY KEY (`utilisateur_id`);
 
 --
--- AUTO_INCREMENT pour les tables exportées
+-- AUTO_INCREMENT for dumped tables
 --
 
 --
--- AUTO_INCREMENT pour la table `createur`
+-- AUTO_INCREMENT for table `createur`
 --
 ALTER TABLE `createur`
   MODIFY `createur_id` int(11) NOT NULL AUTO_INCREMENT;
+
 --
--- AUTO_INCREMENT pour la table `oeuvre`
+-- AUTO_INCREMENT for table `oeuvre`
 --
 ALTER TABLE `oeuvre`
-  MODIFY `oeuvre_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `oeuvre_id` int(11) NOT NULL AUTO_INCREMENT;
+
 --
--- AUTO_INCREMENT pour la table `type`
+-- AUTO_INCREMENT for table `type`
 --
 ALTER TABLE `type`
-  MODIFY `type_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `type_id` int(11) NOT NULL AUTO_INCREMENT;
+
 --
--- AUTO_INCREMENT pour la table `utilisateur`
+-- AUTO_INCREMENT for table `utilisateur`
 --
 ALTER TABLE `utilisateur`
   MODIFY `utilisateur_id` int(11) NOT NULL AUTO_INCREMENT;
+
 --
--- Contraintes pour les tables exportées
+-- Constraints for dumped tables
 --
 
 --
--- Contraintes pour la table `compose`
+-- Constraints for table `compose`
 --
 ALTER TABLE `compose`
   ADD CONSTRAINT `createur_enfant` FOREIGN KEY (`artiste_id`) REFERENCES `createur` (`createur_id`),
   ADD CONSTRAINT `createur_parent` FOREIGN KEY (`groupe_id`) REFERENCES `createur` (`createur_id`);
 
 --
--- Contraintes pour la table `fan_x_createur`
+-- Constraints for table `fan_x_createur`
 --
 ALTER TABLE `fan_x_createur`
   ADD CONSTRAINT `createur` FOREIGN KEY (`createur_id`) REFERENCES `createur` (`createur_id`),
   ADD CONSTRAINT `utilisateur_createur` FOREIGN KEY (`utilisateur_id`) REFERENCES `utilisateur` (`utilisateur_id`);
 
 --
--- Contraintes pour la table `fan_x_oeuvre`
+-- Constraints for table `fan_x_oeuvre`
 --
 ALTER TABLE `fan_x_oeuvre`
   ADD CONSTRAINT `oeuvre` FOREIGN KEY (`oeuvre_id`) REFERENCES `oeuvre` (`oeuvre_id`),
   ADD CONSTRAINT `utilisateur` FOREIGN KEY (`utilisateur_id`) REFERENCES `utilisateur` (`utilisateur_id`);
 
 --
--- Contraintes pour la table `oeuvre`
+-- Constraints for table `oeuvre`
 --
 ALTER TABLE `oeuvre`
   ADD CONSTRAINT `type_oeuvre` FOREIGN KEY (`oeuvre_type_id`) REFERENCES `type` (`type_id`);
