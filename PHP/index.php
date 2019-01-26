449a0b5ccaf1e4cb2fa68c5c14707cfb56ba38a3
Marc Augier <m.augier@me.com>
2019-01-26 10:58:57 +0100

    Page de garde pour appeler les différentes parties du cours
    Oui, c’est du HTML, et il n’y a pas de CSS, désolé

---
+++ PHP/index.php
@@ -0,0 +1,35 @@
+<?php
+/*
+==============================================================================
+
+ Ce script fait partie d'une série d'exemples de code mise à disposition
+  sur https://github.com/marcyves/Developpeur-Web
+
+ (c) 2019 Marc Augier
+
+==============================================================================
+*/
+?>
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="utf-8">
+		<title>Développement Web</title>
+	</head>
+	<body>
+		<h1>Les exemples de script PHP</h1>
+		<ul>
+			<li><a href="bases/">Les bases</a></li>
+			<li><a href="cookie/">Les cookies</a></li>
+			<li><a href="session/">Les sessions</a></li>
+			<li><a href="pwd/">Gérer un mot de passe crypté</a></li>
+	</ul>
+		<h2>Exemples et mini projets</h2>
+		<ul>
+			<li><a href="calculatrice/">Calculatrice</a></li>
+			<li><a href="utilisateur/">Page de connexion d'un utilisateur</a></li>
+			<li><a href="intégration HTML/">PHP et template HTML</a></li>
+		</ul>
+		<h3>
+	</body>
+</html>
