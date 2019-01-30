da6a386716715f94e8fa0549cc7ca506f4462da0
Marc Augier <m.augier@me.com>
2019-01-30 17:33:19 +0100

    Ajout du commentaire de copyright

--- PHP/eshop/entete.php
+++ PHP/eshop/entete.php
@@ -1,3 +1,15 @@
+<?php
+/*
+==============================================================================
+
+ Ce script fait partie d'une série d'exemples de code mise à disposition
+  sur https://github.com/marcyves/Developpeur-Web
+
+ (c) 2019 Marc Augier
+
+==============================================================================
+*/
+?>
 <html>
 <head>
 	<meta charset="utf-8">
